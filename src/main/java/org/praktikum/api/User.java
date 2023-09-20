c2e71125e13ce714a71bd70f95877cd3570556cf
Mike G <kukozi47@gmail.com>
2023-09-21 00:41:27 +0300

    feat: add user class

---
+++ src/main/java/org/praktikum/api/User.java
@@ -0,0 +1,43 @@
+package org.praktikum.api;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class User {
+    private String email;
+    private String name;
+    private String password;
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public static User getRandomUser() {
+        String name = RandomStringUtils.randomAlphabetic(12);
+        String email = RandomStringUtils.randomAlphabetic(8) + "@yandex.ru";
+        String password = RandomStringUtils.randomAlphabetic(6);
+        return new User(email, password, name);
+    }
+}
