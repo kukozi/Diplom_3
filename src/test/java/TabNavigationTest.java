9cac8b62ec494051a43ff3cdb6c874d9faa26efa
Mike G <kukozi47@gmail.com>
2023-09-21 21:09:21 +0300

    refactor: small name changes

--- src/test/java/TabNavigationTest.java
+++ src/test/java/TabNavigationTest.java
@@ -2,7 +2,7 @@ import io.qameta.allure.junit4.DisplayName;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.praktikum.page_object.DesignerPage;
+import org.praktikum.page_object.ConstructorPage;
 
 public class TabNavigationTest extends BaseTest {
     @Before
@@ -13,26 +13,26 @@ public class TabNavigationTest extends BaseTest {
     @Test
     @DisplayName("Check bun icon click")
     public void tabRollsNavigationTest() {
-        DesignerPage designerPage = new DesignerPage(driver);
-        designerPage.clickTabFilling();
-        designerPage.clickTabRolls();
-        Assert.assertEquals("Краторная булка N-200i", designerPage.getRollsName());
+        ConstructorPage constructorPage = new ConstructorPage(driver);
+        constructorPage.clickTabFilling();
+        constructorPage.clickTabRolls();
+        Assert.assertEquals("Краторная булка N-200i", constructorPage.getRollsName());
     }
 
     @Test
     @DisplayName("Check sauces icon click")
     public void tabSaucesNavigationTest() {
-        DesignerPage designerPage = new DesignerPage(driver);
-        designerPage.clickTabSauces();
-        Assert.assertEquals("Соус Spicy-X", designerPage.getSaucesName());
+        ConstructorPage constructorPage = new ConstructorPage(driver);
+        constructorPage.clickTabSauces();
+        Assert.assertEquals("Соус Spicy-X", constructorPage.getSaucesName());
     }
 
     @Test
     @DisplayName("Check filling icon click")
     public void tabFillingNavigationTest() {
-        DesignerPage designerPage = new DesignerPage(driver);
-        designerPage.clickTabFilling();
-        Assert.assertEquals("Биокотлета из марсианской Магнолии", designerPage.getFillingName());
+        ConstructorPage constructorPage = new ConstructorPage(driver);
+        constructorPage.clickTabFilling();
+        Assert.assertEquals("Биокотлета из марсианской Магнолии", constructorPage.getFillingName());
     }
 
 }
